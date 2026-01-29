ae65c413162c4dd1a90fe0363d9201e4f8d65081
ayusha1223 <ayushathapa425@gmail.com>
2026-01-29 14:27:01 +0545

    student model

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -15,6 +15,10 @@ const studentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    imageUrl: {
+  type: String,
+  default: null,
+},
   },
   { timestamps: true }
 );
