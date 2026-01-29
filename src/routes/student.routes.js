02234463b00e523de7f5a674aa6861af5d3f0c47
ayusha1223 <ayushathapa425@gmail.com>
2026-01-29 14:32:13 +0545

    route

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -3,10 +3,18 @@ import {
   registerStudent,
   loginStudent,
 } from "../controllers/student.controller.js";
+import upload from "../config/multer.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerStudent);
 router.post("/login", loginStudent);
+router.post(
+  "/upload-image",
+  authMiddleware,
+  upload.single("image"),
+  uploadStudentImage
+);
 
 export default router;
